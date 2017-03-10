dfbe00c876e4ba608ff700ee0dc4b512459ea5f3
manon643 <manonromain643@hotmail.fr>
2017-03-10 16:30:44 +0100

    Modification du design et ajout de quelques trucs genre : page jouer et jQuery simple

--- js/code.js
+++ js/code.js
@@ -1,7 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(document).ready(function(){
+    $(".jumbotron h1").mouseenter(function(){
+        /*$("body").css("background", "url(../img/Polytechnique.jpg) no-repeat center fixed");*/
+        $(".jumbotron").css("color","black");
+    });
+});
 
