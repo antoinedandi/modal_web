44337bed48c1c882546feb4e3a6a0824db0d4d7c
manon643 <manonromain643@hotmail.fr>
2017-03-31 14:03:42 +0200

    Classe vido avec fonctions correspondantes

--- video.php
+++ video.php
@@ -1,5 +1,69 @@
 <?php
+class Video {
+    /*Classe representant une video de la bdd
+    Les fonctions disponibles sont :  
+     * toString
+     * getVideo(bdd,nom de la vidéo)
+     * insererVideo(bdd,nom,question, bonne réponse, reponse fausse1,reponse fausse2  ,reponse fausse3)      */
+    public $id;
+    public $nom;
+    public $question;
+    public $right_answer;
+    public $wrong_answer1;
+    public $wrong_answer2;
+    public $wrong_answer3;
+ 
+    public function __toString() {
+        return '[video : '.$this->nom.'] ';
+    }
+    
+    public static function getVideoWithID($dbh, $id) {
+        $query = "SELECT * FROM `video` WHERE id = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Video');
+        $sth->execute(array("$id"));
+        $video = $sth->fetch();
+        // $sth : boolean qui dit si ca a marché ou pas
+        $sth->closeCursor();
+        return ($sth)? $video: null;
+    }
+    public static function getVideo($dbh, $nom) {
+        $query = "SELECT * FROM `video` WHERE nom = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Video');
+        $sth->execute(array("$nom"));
+        $video = $sth->fetch();
+        // $sth : boolean qui dit si ca a marché ou pas
+        $sth->closeCursor();
+        return ($sth)? $video: null;
+    }
+    
+    public static function insererVideo($dbh, $nom, $question, $right_answer,$wrong_answer1,$wrong_answer2,$wrong_answer3) {
+        if (Utilisateur::getUtilisateur($dbh, $nom) == null){
+            $sth = $dbh->prepare("INSERT INTO `video` (`nom`, `question`, `right_answer`,`wrong_answer1`,`wrong_answer2`,`wrong_answer3`) VALUES(?,?,?,?,?,?)");
+            $sth->execute(array($nom, $question, $right_answer,$wrong_answer1,$wrong_answer2,$wrong_answer3));
+            return TRUE;
+        }
+        else{
+            echo 'Impossible vidéo déjà ajoutée!!!';
+            return FALSE;
+        }   
+    }
+    
+    public static function testerReponse($dbh,$id,$reponse) {
+        $video=Utilisateur::getVideoWithID($dbh, $id);
+        return $reponse==$test->right_answer;
+    }
+    
 
+    public static function deleteUser($dbh,$login){
+        $query="DELETE FROM `utilisateurs` WHERE `login`=?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Utilisateur');
+        $sth->execute(array("$login"));
+    }
+    
+}
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
