499823ce6179f7c5a90fec9983b2e657013734a8
manon643 <manonromain643@hotmail.fr>
2017-03-31 15:03:10 +0200

    Ajout de la vidéo sur le serveur

--- admin/insererVideo.php
+++ admin/insererVideo.php
@@ -1,16 +1,32 @@
 <?php
 require 'video.php';
 if (isset($_GET["todo"]) && ($_GET["todo"] == "submitVideo")) {
-    $nom = $_POST["nom"];
-    $nom_secure = htmlspecialchars($nom);
-    $question = htmlspecialchars($_POST['question']);
-    $right_answer = htmlspecialchars($_POST['ra']);
-    $wrong_answer1 = htmlspecialchars($_POST['wa1']);
-    $wrong_answer2 = htmlspecialchars($_POST['wa2']);
-    $wrong_answer3 = htmlspecialchars($_POST['wa3']);
     echo "<p class='cadre row'>";
-    if (Video::insererVideo($dbh, $nom, $question, $right_answer, $wrong_answer1, $wrong_answer2, $wrong_answer3)) {
-        echo "Votre vidéo $nom_secure a été ajoutée avec succès";
+
+
+    
+
+    if (!empty($_FILES['fichier']['tmp_name']) && is_uploaded_file($_FILES['fichier']['tmp_name'])) {
+        // Le fichier a bien été téléchargé
+
+/* Téléchargement de la vidéo sur le serveur*/
+        $nom = $_FILES['video']["name"];
+        if (move_uploaded_file($_FILES['fichier']['tmp_name'], 'images/' . $nom)) {
+            echo "Copie réussie";
+        } else {
+            echo "echec de la copie";
+        }
+        /*Ajout de la vidéo sur la base de données*/
+        $nom_secure = htmlspecialchars($nom);
+        $question = htmlspecialchars($_POST['question']);
+        $right_answer = htmlspecialchars($_POST['ra']);
+        $wrong_answer1 = htmlspecialchars($_POST['wa1']);
+        $wrong_answer2 = htmlspecialchars($_POST['wa2']);
+        $wrong_answer3 = htmlspecialchars($_POST['wa3']);
+
+        if (Video::insererVideo($dbh, $nom, $question, $right_answer, $wrong_answer1, $wrong_answer2, $wrong_answer3)) {
+            echo "Votre vidéo $nom_secure a été ajoutée avec succès";
+        }
     } else {
         echo "Il y a eu une erreur dans l'ajout de la vidéo";
     }
@@ -19,7 +35,7 @@ if (isset($_GET["todo"]) && ($_GET["todo"] == "submitVideo")) {
     ?>
     <form class='col-md-12 cadre' action="index.php?todo=submitVideo&page=compte" method='post' enctype="multipart/form-data">
         <label class="control-label">Selectionner une vidéo</label>
-        <input id="video" type="file" class="file">
+        <input name="video" type="file" class="file">
         <label for="question">Question : </label>
         <div class="input-group">
             <span class="input-group-addon">?</span>
@@ -31,7 +47,7 @@ if (isset($_GET["todo"]) && ($_GET["todo"] == "submitVideo")) {
             <span class="input-group-addon"><span class="glyphicons glyphicons-ok" aria-hidden='true'></span></span>
             <input type='text' placeholder="Bonne réponse" name="ra" id="ra">
         </div><br/>
-        
+
         <div class="input-group">
             <span class="input-group-addon"><span class="glyphicons glyphicons-ok"></span></span>
             <input type='text' placeholder="Réponse erronée" name="wa1">
