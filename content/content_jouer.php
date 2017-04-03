a58e2e4e82b56c2ad49a7996a7971d4b528d09ab
Antoine <antoine.dandigne@gmail.com>
2017-04-03 17:35:01 +0200

    modification content_jouer : ordres des questions au hasard

--- content/content_jouer.php
+++ content/content_jouer.php
@@ -8,11 +8,10 @@ function afficher_video($video) {
 CHAINE_DE_FIN;
 }
 
-function afficher_questions($question, $rep1, $rep2, $rep3, $rep4) {
-    $positions = array(1, 2, 3, 4);
-    //print_r($positions);
-    shuffle($positions);
-    //print_r($positions);
+function afficher_questions($question, $rep) {
+    //print_r($rep);
+    shuffle($rep);
+    //print_r($rep);
     echo <<<CHAINE_DE_FIN
         <div class="container-fluid row col-md-8 col-md-offset-2">
         <form class="form-group row cadre" action="?page=jouer" method="post">
@@ -20,26 +19,26 @@ function afficher_questions($question, $rep1, $rep2, $rep3, $rep4) {
           <div class="col-sm-8 col-md-offset-2">
             <div class="form-check">
               <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="choix" value="$rep1" checked>
-                $rep1
+                <input class="form-check-input" type="radio" name="choix" value="$rep[0]" checked>
+                $rep[0]
               </label>
             </div>
             <div class="form-check">
               <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="choix" value="$rep2">
-                $rep2
+                <input class="form-check-input" type="radio" name="choix" value="$rep[1]" >
+                $rep[1]
               </label>
             </div>
             <div class="form-check">
               <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="choix" value="$rep3">
-                $rep3
+                <input class="form-check-input" type="radio" name="choix" value="$rep[2]" >
+                $rep[2]
               </label>
             </div>
             <div class="form-check">
               <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="choix" value="$rep4">
-                $rep4
+                <input class="form-check-input" type="radio" name="choix" value="$rep[3]" >
+                $rep[3]
               </label>
             </div>
             <button type="submit" class="btn btn-default">Valider</button>
@@ -47,11 +46,12 @@ function afficher_questions($question, $rep1, $rep2, $rep3, $rep4) {
         </form>
         </div>
 CHAINE_DE_FIN;
-}
+}   
 
 $form_values_valid = false;
 $id = "1";
 $video = Video::getVideoWithID($dbh, $id);
+$reponses = array($video->right_answer, $video->wrong_answer1, $video->wrong_answer2, $video->wrong_answer3);
 
 //On ne peut accéder au jeu que si on est logé
 if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) {
@@ -74,7 +74,7 @@ FIN;
 
     if (!$form_values_valid) {
         afficher_video($video);
-        afficher_questions($video->question, $video->right_answer, $video->wrong_answer1, $video->wrong_answer1, $video->wrong_answer1);
+        afficher_questions($video->question, $reponses);
     } else {
         echo <<<FIN
         <div class="row col-md-8 col-md-offset-2 cadre">
@@ -90,8 +90,4 @@ else {
 FIN;
     printLoginForm("jouer");
     echo "</div>";
-}
-
-     
-
-
+}   
