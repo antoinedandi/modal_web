3b8cadfc3b38956ba794032257d3209fdea95ea5
Antoine <antoine.dandigne@gmail.com>
2017-04-05 11:29:03 +0200

    un peu de rangement..

--- content/content_welcome.php
+++ content/content_welcome.php
@@ -12,7 +12,7 @@
 
 
     <div class="row col-md-12">         
-        <div class="col-md-6 cadre_transparent">
+        <div class="col-md-7 cadre_transparent">
             <h2>Prochain tirage dans</h2>
             
                 <div class="cell">
@@ -22,7 +22,7 @@
                 </div>
            
         </div> 
-        <div class="col-md-5 col-md-offset-1 cadre_transparent ">
+        <div class="col-md-4 col-md-offset-1 cadre_transparent ">
             <h2>Cagnotte</h2>
             <?php
             $montant = Cagnotte::getMontant($dbh);
